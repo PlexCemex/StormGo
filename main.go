355f825fb2089709aed158b0f5e7296c53bde0ec
PlexCemex <plexcemex@proton.me>
2025-08-29 18:05:53 +1000

    flag is ready

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main(){
-	
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	city := flag.String("city", "", "User city")
+	format := flag.Int("format", 1, "Format for output")
+
+	flag.Parse()
+
+	fmt.Println(*city, *format)
 }
+
+// go run . --city=""
